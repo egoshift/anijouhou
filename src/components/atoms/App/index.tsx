73c2a810aa7afca3b9b5ca837d7ed7a9616d5feb
Sem <12117498+egoshift@users.noreply.github.com>
2021-12-05 11:58:12 +0800

    [Feat][Ems] setup react

---
+++ src/components/atoms/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DiscoverTemplate from 'src/components/templates/DiscoverTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <DiscoverTemplate />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/templates/DiscoverTemplate.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+function DiscoverTemplate(): JSX.Element {
+  return (
+    <>
+      <Button>Test</Button>
+    </>
+  )
+}
+
+export default DiscoverTemplate
